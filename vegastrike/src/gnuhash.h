vsallaberry/privgold-pandora

commit 8fab7754f099ee2ee5814a20af7ed73f1986ffbd

vegastrike/vsUMap (hasttable): remove warnings with old clang c++ libs
* in clang LIBCPP before 3700, the std::hash class is declared as struct
  instead of class for recent versions -> detect it and act
  accordinally.

vegastrike/src/gnuhash.h
@@ -33,6 +33,11 @@
 #if defined(_WIN32) && !defined(__GNUC__) && !defined(_LIBCPP_VERSION)
 # if defined(HAVE_TR1_UNORDERED_MAP) || defined(HAVE_UNORDERED_MAP)
 #  include <unordered_map>  // MSVC doesn't use tr1 dirs
+#  if defined(HAVE_UNORDERED_MAP) && defined(_LIBCPP_VERSION) && (_LIBCPP_VERSION <= 3700)
+#   define GNUHASH_CLASS struct
+#  else
+#   define GNUHASH_CLASS class
+#  endif
 # else
 #  include <hash_map>
 # endif
@@ -54,6 +59,11 @@ namespace stdext {
 # else
 #  if defined(HAVE_UNORDERED_MAP)
 #   include <unordered_map>
+#   if defined(_LIBCPP_VERSION) && (_LIBCPP_VERSION <= 3700)
+#    define GNUHASH_CLASS struct
+#   else
+#    define GNUHASH_CLASS class
+#   endif
 #   include "hashtable.h"
 class Unit;
 namespace std{
@@ -72,7 +82,7 @@ class Unit;
 namespace stdext{
 
 
-  template<> class hash<std::string> {
+  template<> GNUHASH_CLASS hash<std::string> {
   public:
     size_t operator () (const std::string&key) const{
       size_t _HASH_INTSIZE =(sizeof(size_t)*8);
@@ -91,14 +101,14 @@ namespace stdext{
 
 #  endif
 
-  template<> class hash<void *> {
+  template<> GNUHASH_CLASS hash<void *> {
     hash<size_t> a;
   public:
     size_t operator () (const void *key) const{
       return a((size_t)key);
     }
   };
-  template<> class hash<const void *> {
+  template<> GNUHASH_CLASS hash<const void *> {
     hash<size_t> a;
   public:
     size_t operator () (const void * const &key) const{
@@ -106,14 +116,14 @@ namespace stdext{
     }
   };
 
-  template<> class hash<const Unit *> {
+  template<> GNUHASH_CLASS hash<const Unit *> {
     hash<size_t> a;
   public:
     size_t operator () (const Unit * const &key) const{
       return a((size_t)key>>4);
     }
   };
-  template<> class hash<std::pair<Unit *,Unit*> > {
+  template<> GNUHASH_CLASS hash<std::pair<Unit *,Unit*> > {
     hash<size_t> a;
   public:
     size_t operator () (const std::pair<Unit*,Unit*> &key) const{

commit b4df7966ea79bb05417b547d7597192d139fe183

gnuhash: fix build error introduced by 8fab775 with c++98(11 ok)

vegastrike/src/gnuhash.h
@@ -40,12 +40,14 @@
 #  endif
 # else
 #  include <hash_map>
+#  define GNUHASH_CLASS class
 # endif
 #else
 # if __GNUC__ == 2
 #  include <map>
 #  define hash_map map
 #  define stdext std
+#  define GNUHASH_CLASS class
 namespace stdext {
     template<class Key, class Traits = std::less<Key> > class hash_compare
 	{
@@ -69,12 +71,14 @@ class Unit;
 namespace std{
 #  elif defined(HAVE_TR1_UNORDERED_MAP)
 #   include <tr1/unordered_map>
+#   define GNUHASH_CLASS class
 #   include "hashtable.h"
 class Unit;
 namespace std{
 namespace tr1{
 #  else
 #   include <ext/hash_map>
+#   define GNUHASH_CLASS class
 #   define stdext __gnu_cxx
 #   include "hashtable.h"
 