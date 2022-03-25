9e8f2178bc455a71d0e69bc50fbe95061e1a1a5e
vincent <vsallaberry@gmail.com>
2022-03-25 21:31:10 +0100

    tools/hardshortcut: handle windows exec quoting and windows 32bits windres
    * workaround on windres c preprocessor with mingw32 gcc
    
    * fix non-nul-terminated args bug on windows

--- tools/hardshortcut/hardshortcut.c
+++ tools/hardshortcut/hardshortcut.c
@@ -22,18 +22,19 @@
 # include <windows.h>
 # include <process.h>
 # ifdef SHORTCUT_EXECPATH
-#  define doexec(name, ...) _spawnvp(P_NOWAIT, name, __VA_ARGS__)
+#  define doexec(name, ...) execvp(name, __VA_ARGS__) //_spawnvp(P_NOWAIT, name, __VA_ARGS__)
 # else
-#  define doexec(name, ...) _spawnv(P_NOWAIT, name, __VA_ARGS__)
+#  define doexec(name, ...) execv(name, __VA_ARGS__) //_spawnv(P_NOWAIT, name, __VA_ARGS__)
 #endif
-# define EXEC_CONST const
+# define EXEC_CONST // for exec
+//# define EXEC_CONST const // for spawn
 # define PATHSEP ("\\")
 # if defined(__CYGWIN__) || defined(__MINGW__)
 #  include <io.h>
 # else
 #  include <direct.h>
 # endif
-#else
+#else // ! _WIN32
 # include <unistd.h>
 # ifdef SHORTCUT_EXECPATH
 #  define doexec(name, ...) execvp(name, __VA_ARGS__)
@@ -42,7 +43,7 @@
 # endif
 # define EXEC_CONST
 # define PATHSEP ("/")
-#endif
+#endif // ! _WIN32
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdarg.h>
@@ -53,7 +54,10 @@
 #endif
 static EXEC_CONST char * const shortcuts[] = SHORTCUTS;
 
-#if defined(_WINDOWS) && defined(_WIN32)
+#if defined(_WIN32)
+static char ** quote_argv(EXEC_CONST char *const* argv);
+#endif
+#if defined(_WIN32) && defined(_WINDOWS)
 static int ParseCmdLine(const char * cmdline, int * argc, char *** argv);
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR lpCmdLine, int nShowCmd) {
     char ** argv, *argv0;
@@ -65,8 +69,8 @@ int main(int argc, char *argv[]) {
     char * argv0 = strdup(argv[0]);
 #endif
     EXEC_CONST char * const * shortcut = shortcuts;
-    char * basename;
-    char * tmp = argv0 + strlen(argv0) - 1;
+    char * basename, **nargv = NULL, * tmp = argv0 + strlen(argv0) - 1;
+    int ret;
 
     while (tmp >= argv0 && *tmp != '/' && *tmp != *PATHSEP) {
         --tmp;
@@ -85,14 +89,51 @@ int main(int argc, char *argv[]) {
     if (*shortcut == NULL) 
         shortcut = shortcuts;
     fprintf(stderr, "run %s %s\n", shortcut[1], shortcut[2]);
-    if (doexec(shortcut[1], shortcut+1) == -1) {
+#if defined(_WIN32)
+    nargv = quote_argv(shortcut+1);
+#endif
+    if ((ret = doexec(shortcut[1], nargv != NULL ? (EXEC_CONST char *const*) nargv : shortcut+1)) == -1) {
         fprintf(stderr, "error launching %s\n", shortcut[1]);
-        return -1;
     }
-    return 0;
+    if (nargv != NULL) { for (int i = 0; nargv[i] != NULL; ++i) { free(nargv[i]); }; free(nargv); }
+    return ret;
 }
 
-#if defined(_WINDOWS) && defined(_WIN32)
+#if defined(_WIN32)
+static int quote_follows(const char *s, int match0) {
+    while (*s == '\\') ++s;
+    return *s == '"' || (match0 && *s == 0);
+}
+static char ** quote_argv(EXEC_CONST char *const* argv) {
+    // as indicated in microsoft reference for exec*, args must be quoted.
+    int argc;
+    for (argc = 0; argv[argc] != NULL; ++argc) ;/*loop*/
+    char ** nargv = (char **) malloc((argc+1)*sizeof(*argv));
+    if (nargv == NULL)
+        return NULL;
+    nargv[argc] = NULL;
+    for (int i_argv = 0; i_argv < argc; ++i_argv) {
+        size_t len = strlen(argv[i_argv]);
+        char * arg = (char *) malloc((len*2)+3); // worst case is to escape each character.
+        if (arg == NULL) {
+            for (int i = 0; i < i_argv - 1; ++i) free(nargv[i]);
+            free(nargv);
+            return NULL;
+        }
+        *arg = '"';
+        char * parg = arg + 1;
+        for (size_t ic = 0; ic < len; ++ic, ++parg) {
+            if (argv[i_argv][ic] == '\"'
+            ||  (argv[i_argv][ic] == '\\' && (quote_follows(argv[i_argv]+ic,1) || argv[i_argv][ic+1] == 0)))
+                *parg++ = '\\';
+            *parg = argv[i_argv][ic];
+        }
+        strcpy(parg, "\"");
+        nargv[i_argv] = arg;
+    }
+    return nargv;
+}
+# if defined(_WINDOWS)
 static int ParseCmdLine(const char * cmdline, int * pargc, char *** pargv) {
     char *argv0 = (char*) malloc(65535);
     int argc = 1;
@@ -106,16 +147,18 @@ static int ParseCmdLine(const char * cmdline, int * pargc, char *** pargv) {
         argv = (char**) realloc(argv, (argc+2)*sizeof(*argv));
         char * arg = argv[argc++] = lpCmdLine;
         while (*lpCmdLine && (escape || *lpCmdLine != ' ')) {
-            if (*lpCmdLine == '\\' && lpCmdLine[1] == '"') { *arg++ = *(++lpCmdLine); ++lpCmdLine; }
+            if (escape && *lpCmdLine == '\\' && quote_follows(lpCmdLine,0)) { *arg++ = *(++lpCmdLine); ++lpCmdLine; }
             else if (*lpCmdLine == '"') { lpCmdLine++; escape = !escape; }
             else *arg++ = *lpCmdLine++;
         }
-        *arg = *lpCmdLine++ = 0;
+        while (*lpCmdLine == ' ') ++lpCmdLine;
+        *arg = 0;
     }
     argv[argc] = NULL;
     *pargv = argv;
     *pargc = argc;
     return 0;
 }
-#endif
+# endif // ! _WINDOWS
+#endif // ! _WIN32
 
